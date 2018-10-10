2acd0e31d8b96808a83b9bd0377792f5b26c5fa2
Administrator <yangdupeng@chinadiaoyutai.com>
2018-10-10 21:47:34 +0800

    phpserver

--- application/route.php
+++ application/route.php
@@ -9,15 +9,19 @@
 // | Author: liu21st <liu21st@gmail.com>
 // +----------------------------------------------------------------------
 
-return [
-    '__pattern__' => [
-//        'blogs' => '\w+',
-         'blogs' =>  'index/hello'
-    ],
+use think\Route;
+// 注册路由到index模块的News控制器的read操作
+Route::get('/blogs/dds',function(){
+    return 'Hello,world!dds';
+});
 
-//    '[hello]'     => [
+Route::get('/blogs/dd',function(){
+    return 'Hello,world!dd';
+});
+
+
+//return [
+//    '/'     => [
 //        ':id'   => ['index/hello', ['method' => 'get'], ['id' => '\d+']],
-//        ':name' => ['index/hello', ['method' => 'post']],
-//    ],
-
-];
+//    ]
+//];
